1971e78db336583bddbb8a60833471cd3255fa78
Mayara Madeira Trevisol <mayara.trevisol@udesc.br>
2022-04-05 17:53:46 -0300

    update the README.md

--- fundsfinder/urls.py
+++ fundsfinder/urls.py
@@ -20,15 +20,16 @@ from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
     path('api/v1/', include('api.urls', namespace='api')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api_schema/', get_schema_view(
         title='API Schema',
         description='Guide for the REST API'
     ), name='api_schema'),
     path('docs/', TemplateView.as_view(
-            template_name='docs.html',
-            extra_context={'schema_url':'api_schema'}
-            ), name='swagger-ui'),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+        template_name='docs.html',
+        extra_context={'schema_url': 'api_schema'}
+    ), name='swagger-ui'),
+
     path('admin/', admin.site.urls),
 
 ]
