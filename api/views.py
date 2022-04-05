1b199f1acdd3ad11852224948753e080d47df637
06244806905 <mayara.trevisol@udesc.br>
2022-04-05 20:48:35 -0300

    Adiciona filtro, busca textual e ordenação

--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from api.serializers import FundoImobiliarioSerializer
 from rest_framework import viewsets, permissions
 from api.models import FundoImobiliario
@@ -8,5 +8,12 @@ from api.models import FundoImobiliario
 class FundoImobiliarioViewSet(viewsets.ModelViewSet):
   queryset = FundoImobiliario.objects.all()
   serializer_class = FundoImobiliarioSerializer
-  filter_backends = [DjangoFilterBackend]
+  filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
   filterset_fields = ['codigo', 'setor']
+  search_fields = ['codigo', 'setor']
+  ordering_fields = [
+    'dividend_yield_medio_12m',
+    'setor',
+    'vacancia_financeira',
+    'vacancia_fisica',
+    'quantidade_ativos']
