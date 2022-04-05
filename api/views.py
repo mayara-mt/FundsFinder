bab7d112b0574ef5680af6cf1cc1c2267c4d0044
06244806905 <mayara.trevisol@udesc.br>
2022-04-05 20:35:58 -0300

    .gitignore is now working

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
 from api.serializers import FundoImobiliarioSerializer
 from rest_framework import viewsets, permissions
 from api.models import FundoImobiliario
@@ -7,3 +8,5 @@ from api.models import FundoImobiliario
 class FundoImobiliarioViewSet(viewsets.ModelViewSet):
   queryset = FundoImobiliario.objects.all()
   serializer_class = FundoImobiliarioSerializer
+  filter_backends = [DjangoFilterBackend]
+  filterset_fields = ['codigo', 'setor']
--- fundsfinder/settings.py
+++ fundsfinder/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_swagger',
+    'django_filters',
     'fundsfinder',
     'api'
 ]
